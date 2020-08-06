d4d0e1986d46e77607a68e7db8d578039ab4aa8d
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-06 13:12:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School
+
+  attr_accessor ()
+
+  ROSTER = {}
+
+  def initialize(school_name)
+    @school_name = school_name
+  end
+
+  def add_student(student_name, grade)
+    @student_name = student_name
+    @grade = grade
+    ROSTER << {grade: [student_name]}
+  end
+
+end
