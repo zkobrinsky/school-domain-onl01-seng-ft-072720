901ec98c7143b5db98ad58a2ec26f1326c66e4cc
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-06 14:44:43 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -45,7 +45,7 @@ attr_accessor :roster
     hash = @roster.sort_by {|grade, name| grade}.to_h
     hash.each {|grade, name| name = name.sort}
     hash
-    binding.pry
+    # binding.pry
   end
 
 end
