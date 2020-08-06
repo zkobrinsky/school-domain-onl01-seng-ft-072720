ac4fe42bf8e717c39c18306cf3b5096764fd6a1c
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-06 13:25:03 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -21,7 +21,9 @@ class School
   def add_student(student_name, grade)
     @student_name = student_name
     @grade = grade
+    if ROSTER !include? grade
     ROSTER[grade] = []
+    end
     ROSTER[grade] << "#{student_name}"
     # binding.pry
   end
